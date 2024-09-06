22ee96776a6546cff4c8b2ee3047db191e9305b5
ATRi111 <1657125180@qq.com>
2024-09-06 14:16:18 +0800

    VBO,VAO

--- Test/Program.cpp
+++ Test/Program.cpp
@@ -1,3 +1,4 @@
+#include "glad.h"
 #include "glfw3.h"
 
 int main(void)
@@ -16,14 +17,36 @@ int main(void)
 
     glfwMakeContextCurrent(window);
 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        glfwTerminate();
+        return -1;
+    }
+
     while (!glfwWindowShouldClose(window))
     {
-
         glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void DrawTriangle()
+{
+    float positions[] = {
+        -0.5f,-0.5f,
+        0.5f,-0.5f,
+        0.0f,0.5f
+    };
+
+    GLuint bufferIndex;
+    glGenBuffers(1, &bufferIndex);
+    glBindBuffer(GL_ARRAY_BUFFER, bufferIndex);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, positions, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 }
